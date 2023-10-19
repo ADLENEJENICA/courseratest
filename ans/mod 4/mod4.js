84eafde7f296f7a85dc82ff4c920f0f939ebeba0
Shifanijoseph <147128029+Shifanijoseph@users.noreply.github.com>
2023-10-19 19:34:45 +0530

    updated

---
+++ ans/mod 4/mod4.js
@@ -0,0 +1,18 @@
+var names=new Array();
+names[0]="joseph";
+names[1]="shifani";
+names[2]="joan";
+names[3]="jenicka";
+names[4]="Akash";
+names[5]="joal";
+names[6]="shanky";
+names[7]="sarmi";
+for(var i=0;i<names.length;i++){
+    if(names[i].charAt(0)==='J'||names[i].charAt(0)==='j'){
+        console.log("Goodbye"+names[i])
+
+    }
+    else{
+        console.log("Hello "+names[i])
+    }
+}
